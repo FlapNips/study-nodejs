0dcabd2819b5cc603deb37a1f8877d4ed77e9908
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-01 20:33:07 -0300

    NODEJS - Iniciando Aula 16

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get("/", (req,res)=> res.send("FOIII"))
+app.get("/", (req,res)=> res.send("Send Main home"))
+app.get("/sobre/:nome/:sobrenome", function(req,res){
+		res.send("Olá "+req.params.nome)
+})
+app.get("/blog", (req,res)=> res.send("Send blog"))
+
 
 app.listen(8080, ()=> console.log('Servidor rodando !'))
