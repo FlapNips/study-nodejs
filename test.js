02a29750ae788af0a93007408ee47613d5983321
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-01 22:04:26 -0300

    NODEJS - Iniciando aula 18

---
+++ test.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('treinamento', 'postgres', '', {
+	host: 'localhost',
+	dialect: 'postgres'
+})
+
+sequelize.authenticate()
+.then(()=>
+console.log('Sequelize conectado com sucesso')
+)
+.catch((error) =>
+console.log(`Erro ao se conectar: ${error}`)
+)
